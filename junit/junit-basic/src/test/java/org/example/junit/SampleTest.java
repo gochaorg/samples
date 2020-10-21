45ae0dec5b53792ca9feb7c490b555a0fe465e86
Kamnev Georgiy <nt.gocha@gmail.com>
2020-10-22 03:45:30 +0500

    test tags

--- junit/junit-basic/src/test/java/org/example/junit/SampleTest.java
+++ junit/junit-basic/src/test/java/org/example/junit/SampleTest.java
@@ -1,8 +1,9 @@
 package org.example.junit;
 
-import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
@@ -11,7 +12,7 @@ import java.util.Random;
 public class SampleTest {
     public Random random = new Random();
 
-    public int summ( int a, int b ){
+    public int summV1(int a, int b ){
         int r = random.nextInt(10);
         if( r!=0 ){
             return a+b;
@@ -20,10 +21,16 @@ public class SampleTest {
         }
     }
 
+    public int summV2(int a, int b ){
+        return a+b;
+    }
+
+    @Tag(T.UnStable)
     @DisplayName("summ with random bug")
     @Test
-    public void summ(){
-        System.out.println("summ with random bug");
+    public void summV1(){
+        System.out.println("summV1()");
+        System.out.println("========");
 
         for( int i=0; i<20; i++ ) {
             System.out.println("cycle "+i);
@@ -31,7 +38,29 @@ public class SampleTest {
             int a = random.nextInt(10) + 1;
             int b = random.nextInt(10) + 1;
             int expect = a + b;
-            int result = summ(a, b);
+            int result = summV1(a, b);
+            boolean matched = expect == result;
+
+            String message = "summ( " + a + ", " + b + " ) => { expect=" + expect + " result=" + result + " }";
+            System.out.println(message);
+            assertTrue(matched, message);
+        }
+    }
+
+    @Tag(T.Stable)
+    @Tag(T.Atomic)
+    @Test
+    public void summV2(){
+        System.out.println("summV2()");
+        System.out.println("========");
+
+        for( int i=0; i<20; i++ ) {
+            System.out.println("cycle "+i);
+
+            int a = random.nextInt(10) + 1;
+            int b = random.nextInt(10) + 1;
+            int expect = a + b;
+            int result = summV2(a, b);
             boolean matched = expect == result;
 
             String message = "summ( " + a + ", " + b + " ) => { expect=" + expect + " result=" + result + " }";
---
+++ junit/junit-basic/src/test/java/org/example/junit/T.java
@@ -0,0 +1,18 @@
+package org.example.junit;
+
+public interface T {
+    /**
+     * Стабильные баги
+     */
+    final static String Stable = "Stable";
+
+    /**
+     * Не стабильные баги, вероятность бага Б= 10%, тестировать по 10 или более раз
+     */
+    final static String UnStable = "UnStable";
+
+    /**
+     * Тест не требует предварительной настройки
+     */
+    final static String Atomic = "Atomic";
+}
