520401e9b5375fcc751f622b4e544e60925d3efd
Kamnev Georgiy <nt.gocha@gmail.com>
2020-10-21 21:15:10 +0500

    simple junit test

---
+++ junit/junit-basic/src/test/java/org/example/junit/SampleTest.java
@@ -0,0 +1,15 @@
+package org.example.junit;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @Test
+    public void simple(){
+        System.out.println("simple");
+        System.out.println("simple");
+        System.out.println("simple");
+        System.out.println("simple");
+        System.out.println("simple");
+        System.out.println("simple");
+    }
+}
