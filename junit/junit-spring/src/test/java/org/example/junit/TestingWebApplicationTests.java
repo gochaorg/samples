c1911a7d133090b14c7e8f3dcba2064642af40f0
Kamnev Georgiy <nt.gocha@gmail.com>
2020-10-26 20:10:32 +0500

    mock without using socket

--- junit/junit-spring/src/test/java/org/example/junit/TestingWebApplicationTests.java
+++ junit/junit-spring/src/test/java/org/example/junit/TestingWebApplicationTests.java
@@ -1,29 +1,27 @@
 package org.example.junit;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TestingWebApplicationTests {
     @Autowired
-    private HomeController controller;
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
-    public void contextLoads() {
-        String url = "http://localhost:" + port + "/";
-        System.out.println("try get "+url);
-        assertThat(this.restTemplate.getForObject(url,
-                String.class)).contains("Hello, World");
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
     }
 }
