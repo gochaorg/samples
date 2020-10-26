c3e0b96b456a281bc4ed11af95394064f9433d3c
Kamnev Georgiy <nt.gocha@gmail.com>
2020-10-26 20:08:25 +0500

    with use socket

--- junit/junit-spring/src/test/java/org/example/junit/TestingWebApplicationTests.java
+++ junit/junit-spring/src/test/java/org/example/junit/TestingWebApplicationTests.java
@@ -4,14 +4,26 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TestingWebApplicationTests {
     @Autowired
     private HomeController controller;
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     public void contextLoads() {
-        assertThat(controller).isNotNull();
+        String url = "http://localhost:" + port + "/";
+        System.out.println("try get "+url);
+        assertThat(this.restTemplate.getForObject(url,
+                String.class)).contains("Hello, World");
     }
 }
